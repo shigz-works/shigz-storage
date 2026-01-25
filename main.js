e437ccde2f554f1fcbb02fc03a8935a20ec5be0c
Shigz <shigeruslayer12345@gmail.com>
2026-01-25 09:49:40 +0800

    Update main.js

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ let avatarRoot = null;
 let faceMesh = null;
 
 const loader = new GLTFLoader();
-loader.load("./avatar.glb", (gltf) => {
+loader.load("./avatar1.glb", (gltf) => {
   avatarRoot = gltf.scene;
   scene.add(avatarRoot);
 
