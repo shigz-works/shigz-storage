8d8c8872ee2d8626377585e2a49e91ef3e0ed2d3
Shigz <shigeruslayer12345@gmail.com>
2026-01-26 11:14:25 +0800

    Update main.js

--- main.js
+++ main.js
@@ -31,16 +31,15 @@ camera.lookAt(0, 1.6, 0);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.outputColorSpace = THREE.SRGBColorSpace;
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 1.8;
+renderer.toneMapping = THREE.NoToneMapping;
 document.body.appendChild(renderer.domElement);
 
 /* =========================
   LIGHTING
 ========================= */
-scene.add(new THREE.AmbientLight(0xffffff, 0.5));
-scene.add(new THREE.HemisphereLight(0xffffff, 0xcccccc, 0.7));
-const dirLight = new THREE.DirectionalLight(0xffffff, 0.9);
+scene.add(new THREE.AmbientLight(0xffffff, 1.0));
+scene.add(new THREE.HemisphereLight(0xffffff, 0xffffff, 0.8));
+const dirLight = new THREE.DirectionalLight(0xffffff, 1.2);
 dirLight.position.set(0, 5, 2);
 scene.add(dirLight);
 
